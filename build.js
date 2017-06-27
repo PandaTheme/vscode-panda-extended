76baaabae59a657eeabac891a0cab04a9c96acc3
Hedgecock <r628853@TRG516359.local>
2017-06-27 16:02:03 -0700

    Theme regex and merge conflicts

--- build.js
+++ build.js
@@ -9,12 +9,13 @@ let base = yaml.safeLoad(readFileSync('themes/panda-base.yaml', 'utf-8'));
 const workbench = yaml.safeLoad(readFileSync('themes/workbench.yaml', 'utf-8'));
 const template = yaml.safeLoad(readFileSync('themes/template.yaml', 'utf-8'));
 const markdown = yaml.safeLoad(readFileSync('themes/markdown.yaml', 'utf-8'));
+const regex = yaml.safeLoad(readFileSync('themes/regex.yaml', 'utf-8'));
 const jsdoc = yaml.safeLoad(readFileSync('themes/jsdoc.yaml', 'utf-8'));
 
 // Merge workbench styles
 Object.assign(base, workbench);
 // Merge additional syntax token styles
-base.tokenColors = base.tokenColors.concat(template, markdown, jsdoc);
+base.tokenColors = base.tokenColors.concat(template, markdown, regex, jsdoc);
 
 // Stringify all of the combined theme styles so we can run string regexes on it to
 // replace color variables with color values
