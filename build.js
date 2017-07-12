a0221101a5f76ad77f41fc562e4a8005eb03772a
Siamak Mokhtari <s.mokhtari75@gmail.com>
2017-07-12 17:23:36 +0430

    Improved Workbench panda theme

--- build.js
+++ build.js
@@ -10,13 +10,14 @@ const workbench = yaml.safeLoad(readFileSync('themes/workbench.yaml', 'utf-8'));
 const template = yaml.safeLoad(readFileSync('themes/template.yaml', 'utf-8'));
 const markdown = yaml.safeLoad(readFileSync('themes/markdown.yaml', 'utf-8'));
 const html = yaml.safeLoad(readFileSync('themes/html.yaml', 'utf-8'));
+const css = yaml.safeLoad(readFileSync('themes/css.yaml', 'utf-8'));
 const regex = yaml.safeLoad(readFileSync('themes/regex.yaml', 'utf-8'));
 const jsdoc = yaml.safeLoad(readFileSync('themes/jsdoc.yaml', 'utf-8'));
 
 // Merge workbench styles
 Object.assign(base, workbench);
 // Merge additional syntax token styles
-base.tokenColors = base.tokenColors.concat(template, markdown, html, regex, jsdoc);
+base.tokenColors = base.tokenColors.concat(template, markdown, html, css, regex, jsdoc);
 
 // Stringify all of the combined theme styles so we can run string regexes on it to
 // replace color variables with color values
